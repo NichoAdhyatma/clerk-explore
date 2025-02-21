e8b8b8177d317ef4aa6a883230b0c6938046a291
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-02-21 19:10:35 +0700

    ui adjust

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function page() {
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-4">
-      <UserButton showName />
+      <UserButton showName  />
       <p>Admin Page</p>
     </div>
   );
