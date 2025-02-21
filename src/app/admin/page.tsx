27297b9ad1ec1b1e0333980403645075139e17af
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-02-21 16:45:45 +0700

    new logout button

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,12 @@ import React from "react";
 export default function page() {
   return (
     <div>
-      Admin Page <SignOutButton redirectUrl="/sign-in" />
+      Admin Page{" "}
+      <SignOutButton redirectUrl="/sign-in">
+        <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:cursor-pointer">
+          Sign Out
+        </button>
+      </SignOutButton>
     </div>
   );
 }
